747a4007df19c540ef2cc35633ee9963f083f795
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-07-22 15:55:41 +0600

    update db connection

--- moodlev2-server-app/models/db_getCurrentCoursesById.js
+++ moodlev2-server-app/models/db_getCurrentCoursesById.js
@@ -1,89 +1,24 @@
-const {Client} = require('pg');
-const client = new Client ({
-    host: "localhost",
-    user: "postgres",
-    port: "5432",
-    password: "1705092pgsql",
-    database : "MoodleV2"
-});
+const Pool = require('pg').Pool;
+require('dotenv').config();
 
-// const getCurrentCoursesById = (studentId) => {
-//     console.log('db getCurrentCoursesById');
-//     client.connect().then(() => {
-//         console.log('Connected to database');
-//         client.query('select json_agg(t) FROM get_current_course($1) as t',[studentId],(err,res)=>{
-//             if(err)
-//             {
-//                 console.log(err.message);
-//                 client.end();
-//                 return null;
-//             }
-//             else
-//             {
-//                 console.log('db function success case');
-//                 result = res.rows[0].json_agg;
-//                 client.end();
-//                 console.log(result);
-//                 console.log('db function after client end');
-//                 return result;
-//             }
-//         })
-//     });
+const devConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+};
+
+// const prodConfig = {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
 // };
 
-// exports.getCurrentCoursesById = getCurrentCoursesById;
+// const pool = new Pool(
+//     process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+// );
+const pool = new Pool(devConfig);
 
-
-let result;
-const studentId = 1705119;
-console.log('db getCurrentCoursesById');
-client.connect().then(() => {
-    console.log('Connected to database');
-    client.query('select json_agg(t) FROM get_current_course($1) as t',[studentId],(err,res)=>{
-        if(err)
-        {
-            console.log(err.message);
-            client.end;
-            result = null;
-        }
-        else
-        {
-            console.log('db function success case');
-            result = res.rows[0].json_agg;
-            client.end;
-            console.log(result);
-            console.log('db function after client end');
-            
-        }
-    })
-});
-
-
-// const { Pool } = require('pg')
-// const pool = new Pool({
-//     host: "localhost",
-//     user: "postgres",
-//     port: "5432",
-//     password: "1705092pgsql",
-//     database : "MoodleV2"
-// })
-// // the pool will emit an error on behalf of any idle clients
-// // it contains if a backend error or network partition happens
-// pool.on('error', (err, client) => {
-// console.error('Unexpected error on idle client', err)
-// process.exit(-1)
-// })
-// let result;
-// // callback - checkout a client
-// pool.connect((err, client, done) => {
-// if (err) throw err
-// client.query('select json_agg(t) FROM get_current_course($1) as t',[studentId],(err,res) => {
-//     done()
-//     if (err) {
-//     console.log(err.stack)
-//     } else {
-//     console.log(res.rows[0].json_agg)
-//     }
-// })
-// })
-
+module.exports = pool;
