9b83ab7b771fdf08f7ea5b3af0997d84c995367e
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2022-07-31 18:49:10 +0600

    Dumped

--- moodlev2-db/Moodle-Queries.sql
+++ moodlev2-db/Moodle-Queries.sql
@@ -5,10 +5,10 @@ insert into student
 values (1,'Md. Shariful Islam','4149064daa97438c2dac602c7540e4eba55a353dd0611b3eac610bb66ad34e3b',2017,119,5);
 
 insert into course
-values(1,'Introduction to Computer Programming',1,5,2018,1,1);
+values(1,'Introduction to Computer Programming',1,5,5,2017,2018,1,1);
 
 insert into course
-values(2,'Data Structures and Algorithms',3,5,2018,2,1);
+values(2,'Data Structures and Algorithms',3,5,5,2017,2018,2,1);
 
 insert into section(section_no, section_name, course_id)
 values(1,'CSE-2017-B2-CSE101-2018',1);
