2458dbec8b0f05137c289f8d5f74c49283aed53c
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-08-11 12:20:53 +0600

    unused import remove

--- moodlev2-client-react-app/src/admin/components/add_course.js
+++ moodlev2-client-react-app/src/admin/components/add_course.js
@@ -1,8 +1,6 @@
 import Select from 'react-select';
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { Button } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
 
 const CourseAddForm = ({course})=>{
     // const [selectedOption, setSelectedOption] = useState(null);
