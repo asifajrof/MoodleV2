60a026d73c607bc91a43b6b6accaffec54531dd2
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2022-08-12 16:33:34 +0600

    Hashing algorithm is fixed in DB side. Query file has a sample for add_course, add_student etc. Use these inside NodeJS. There is and admin account too

--- moodlev2-db/Moodle-Queries.sql
+++ moodlev2-db/Moodle-Queries.sql
@@ -1,17 +1,71 @@
-insert into department
-values (5,'Computer Science and Engineering','CSE');
+insert into department(dept_code, dept_shortname, dept_name)
+values (1,'Arch','Architecture');
 
-insert into student
-values (default,'Md. Shariful Islam','4149064daa97438c2dac602c7540e4eba55a353dd0611b3eac610bb66ad34e3b',2017,119,5);
+insert into department(dept_code, dept_shortname, dept_name)
+values (2,'ChE','Chemical Engineering');
 
-insert into course
-values(default,'Introduction to Computer Programming',1,5,5,2017,2018,1,1);
+insert into department(dept_code, dept_shortname, dept_name)
+values (4,'CE','Civil Engineering');
 
-insert into course
-values(default,'Data Structures and Algorithms',3,5,5,2017,2018,2,1);
+insert into department(dept_code, dept_shortname, dept_name)
+values (5,'CSE','Computer Science and Engineering');
+
+insert into department(dept_code, dept_shortname, dept_name)
+values (6,'EEE','Electrical and Electronic Engineering');
+
+insert into department(dept_code, dept_shortname, dept_name)
+values (8,'IPE','Industrial and Production Engineering');
+
+insert into department(dept_code, dept_shortname, dept_name)
+values (10,'ME','Mechanical Engineering');
+
+insert into department(dept_code, dept_shortname, dept_name)
+values (11,'MME','Materials and Metallurgical Engineering');
+
+insert into department(dept_code, dept_shortname, dept_name)
+values (15,'URP','Urban and Regional Planning');
+
+insert into department(dept_code, dept_shortname, dept_name)
+values (18,'BME','Biomedical Engineering');
+
+do
+$$
+begin
+    execute add_admin('Nazmul Haque','nazmuladmin','$2a$12$a3sA/qgphlpSnjZl2EkaW.EvlHrTbsrX8ZjD1iqJzqZgYLdkPXmAy',null);
+end;
+$$;
+
+
+do
+$$
+begin
+    execute add_student('Md. Shariful','$2a$12$BjvAnEmgjQsjLxPmFsD8Peh7H0DV49ZiXZisriA/IZ92ySlPJc1p.',119,5,2017,null);
+end;
+$$;
+
+do
+$$
+begin
+    execute add_course('Fault Tolerant Systems',23,5,5,2017,2022,4,1);
+end;
+$$;
+
+do
+$$
+begin
+    execute add_course('High Performance Database System',53,5,5,2017,2022,4,1);
+end;
+$$;
+
+do
+$$
+begin
+    execute add_teacher('A.B.M. Alim Al Islam','alimrazi','$2a$12$vJa9O6JwQkfILdYj5bSsCuVbeSC863/MIe7x1UZM0qU7iPcwfewB2',5,null);
+end
+$$;
 
 insert into section(section_no, section_name, course_id)
-values(default,'CSE-2017-B2-CSE101-2018',1);
+values(default,'CSE-2017-B2-CSE423-2022',1);
 
 insert into enrolment(student_id, section_id)
 values (1,1);
