0d9b178ef4bf0d48b357d6b396463ef834d9a04d
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-08-15 16:26:23 +0600

    monthly calendar

--- moodlev2-server-app/routes/calendar-routes.js
+++ moodlev2-server-app/routes/calendar-routes.js
@@ -5,5 +5,6 @@ const calendarControllers = require("../controllers/calendar-controllers");
 const calendarRoutes = express.Router();
 
 calendarRoutes.post("/mini", calendarControllers.getMarkDateList);
+calendarRoutes.post("/month", calendarControllers.getEventListMonthView);
 
 module.exports = calendarRoutes;
