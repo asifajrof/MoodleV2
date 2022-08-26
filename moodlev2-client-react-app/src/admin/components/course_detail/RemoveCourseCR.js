40689fbeabe1983be69048db010f4034896628ad
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-08-26 18:36:05 +0600

    admin remove course teacher student cr

---
+++ moodlev2-client-react-app/src/admin/components/course_detail/RemoveCourseCR.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const RemoveCourseCR = ({ courseId, CRList }) => {
+  const removeCourseCR = async (removeObj) => {
+    try {
+      const res = await fetch(`/api/admin/removeCourseCR`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(removeObj),
+      });
+      const data = await res.json();
+
+      console.log(data);
+      console.log(res.status);
+
+      if (res.status === 200) {
+        console.log("CR removed successfully!");
+      } else {
+        console.log(data.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const onDeleteClick = () => {
+    const userNameList = CRList.map((cr) => cr.crObj.uname);
+    const removeObj = {
+      courseId: courseId,
+      crList: userNameList,
+    };
+    // console.log(removeObj);
+    removeCourseCR(removeObj);
+  };
+  return (
+    <>
+      <Button
+        variant="outlined"
+        startIcon={<DeleteIcon />}
+        onClick={onDeleteClick}
+      >
+        Delete
+      </Button>
+      <br />
+      <br />
+    </>
+  );
+};
+
+export default RemoveCourseCR;
