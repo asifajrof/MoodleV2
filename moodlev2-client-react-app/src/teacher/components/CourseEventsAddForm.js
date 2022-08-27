a1adadb000be6d47f5995467775622d7d7e2179a
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-08-27 16:24:55 +0600

    section select in event add

--- moodlev2-client-react-app/src/teacher/components/CourseEventsAddForm.js
+++ moodlev2-client-react-app/src/teacher/components/CourseEventsAddForm.js
@@ -12,6 +12,29 @@ import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { ListItemIcon, ListItemText, Checkbox } from "@mui/material";
+
+// import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@mui/styles";
+
+// const useStyles = makeStyles((theme) => ({
+//   formControl: {
+//     margin: theme.spacing(1),
+//     width: 300,
+//   },
+//   indeterminateColor: {
+//     color: "#f50057",
+//   },
+//   selectAllText: {
+//     fontWeight: 500,
+//   },
+//   selectedAll: {
+//     backgroundColor: "rgba(0, 0, 0, 0.08)",
+//     "&:hover": {
+//       backgroundColor: "rgba(0, 0, 0, 0.08)",
+//     },
+//   },
+// }));
 
 const typeList = [
   {
@@ -36,11 +59,46 @@ const typeList = [
   },
 ];
 
+const sectionList = [
+  {
+    section_no: 1,
+    section_name: "CSE-2017-B2-CSE423-2022",
+  },
+  {
+    section_no: 2,
+    section_name: "CSE-2017-B1-CSE423-2022",
+  },
+  {
+    section_no: 3,
+    section_name: "CSE-2017-A2-CSE423-2022",
+  },
+  {
+    section_no: 4,
+    section_name: "CSE-2017-A1-CSE423-2022",
+  },
+];
+
 const CourseEventsAddForm = ({ userName, courseId }) => {
+  //   const classes = useStyles();
   const navigate = useNavigate();
   const [eventType, setEventType] = useState("");
+  const [eventSection, setEventSection] = useState([]);
+  // const [eventSectionName, setEventSectionName] = useState([]);
   const [eventDescription, setEventDescription] = useState("");
   const [eventTime, setEventTime] = useState(new Date());
+  const isAllSelected =
+    sectionList.length > 0 && eventSection.length === sectionList.length;
+
+  const eventSectionChange = (event) => {
+    const value = event.target.value;
+    if (value[value.length - 1] === "all") {
+      setEventSection(
+        eventSection.length === sectionList.length ? [] : sectionList // sectionList.map((s) => s.section_no)
+      );
+      return;
+    }
+    setEventSection(value);
+  };
 
   const addCourseForum = async (courseForumObj) => {
     //post method here
@@ -73,18 +131,21 @@ const CourseEventsAddForm = ({ userName, courseId }) => {
   };
   const onSubmitAction = (event) => {
     event.preventDefault();
+    const eventSectionList = eventSection.map((s) => s.section_no);
     const courseEventObj = {
       eventType: eventType,
       eventTime: eventTime,
       eventDescription: eventDescription,
       courseId: courseId,
       teacherUserName: userName,
+      eventSectionList: eventSectionList,
     };
     console.log(courseEventObj);
 
     setEventType("");
     setEventDescription("");
     setEventTime(new Date());
+    setEventSection([]);
 
     // addCourseForum(courseForumObj);
 
@@ -121,44 +182,96 @@ const CourseEventsAddForm = ({ userName, courseId }) => {
             <></>
           ) : (
             <>
-              {eventType == 5 ? (
-                <></>
-              ) : (
-                <div>
-                  schedule button
-                  <br /> <br />
-                </div>
-              )}
-
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateTimePicker
-                  renderInput={(props) => <TextField {...props} />}
-                  label="Select Event Time"
-                  value={eventTime}
-                  onChange={(newValue) => {
-                    setEventTime(newValue);
-                  }}
-                />
-              </LocalizationProvider>
-              <br />
-              <br />
-              <InputLabel>Description</InputLabel>
-              <TextField
+              <InputLabel id="mutiple-select-label">Select Section</InputLabel>
+              <SelectMui
                 fullWidth
-                type="text"
-                id="addcoursetopic__topic__desc"
-                // label="Description"
-                variant="outlined"
-                multiline
-                value={eventDescription}
-                onChange={(e) => setEventDescription(e.target.value)}
-                required
-              />
-              <br />
-              <br />
-              <Button type="submit" variant="outlined">
-                Add
-              </Button>
+                labelId="mutiple-select-label"
+                multiple
+                value={eventSection}
+                onChange={eventSectionChange}
+                renderValue={(eventSection) => {
+                  let str = "";
+                  eventSection.forEach((section) => {
+                    str += section.section_name + ", ";
+                  });
+                  return str;
+                }}
+                // MenuProps={MenuProps}
+              >
+                <MenuItem
+                  value="all"
+                  //   classes={{
+                  //     root: isAllSelected ? classes.selectedAll : "",
+                  //   }}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      //   classes={{ indeterminate: classes.indeterminateColor }}
+                      checked={isAllSelected}
+                      indeterminate={
+                        eventSection.length > 0 &&
+                        eventSection.length < sectionList.length
+                      }
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    // classes={{ primary: classes.selectAllText }}
+                    primary="Select All"
+                  />
+                </MenuItem>
+                {sectionList.map((option, index) => (
+                  <MenuItem key={index} value={option}>
+                    <ListItemIcon>
+                      <Checkbox checked={eventSection.indexOf(option) > -1} />
+                    </ListItemIcon>
+                    <ListItemText primary={option.section_name} />
+                  </MenuItem>
+                ))}
+              </SelectMui>
+              {eventSection === "" ? (
+                <> </>
+              ) : (
+                <>
+                  {eventType === 5 ? (
+                    <></>
+                  ) : (
+                    <div>
+                      schedule button
+                      <br /> <br />
+                    </div>
+                  )}
+
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                      renderInput={(props) => <TextField {...props} />}
+                      label="Select Event Time"
+                      value={eventTime}
+                      onChange={(newValue) => {
+                        setEventTime(newValue);
+                      }}
+                    />
+                  </LocalizationProvider>
+                  <br />
+                  <br />
+                  <InputLabel>Description</InputLabel>
+                  <TextField
+                    fullWidth
+                    type="text"
+                    id="addcoursetopic__topic__desc"
+                    // label="Description"
+                    variant="outlined"
+                    multiline
+                    value={eventDescription}
+                    onChange={(e) => setEventDescription(e.target.value)}
+                    required
+                  />
+                  <br />
+                  <br />
+                  <Button type="submit" variant="outlined">
+                    Add
+                  </Button>
+                </>
+              )}
             </>
           )}
         </form>
