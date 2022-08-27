6988465c8eeb4fb8cd3212ef502754c76f46e7f1
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-08-27 10:54:24 +0600

    teacher event add form init

--- moodlev2-client-react-app/src/teacher/pages/course_events.js
+++ moodlev2-client-react-app/src/teacher/pages/course_events.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 // import { useLocation } from 'react-router-dom';
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 import CourseMenuBar from "../components/course_menu_bar";
 import CourseEvaluationEvents from "../components/course_evaluation_events";
@@ -34,8 +36,20 @@ const TeacherCourseEvents = ({ userName }) => {
           {courseInfo._course_code}: {courseInfo._course_name}
         </div>
         <div className="course__home__container__divider">
-          {/* <FileUpload /> */}
-          <CourseEvaluationEvents userName={userName} courseId={courseId} />
+          <div className="course__home__item__left">
+            <div
+              className="course__container__add"
+              style={{ paddingRight: "4rem" }}
+            >
+              <Link to={`/course/${courseId}/events/addnew`}>
+                <Button variant="contained">
+                  Add New
+                  <AddIcon />
+                </Button>
+              </Link>
+            </div>
+            <CourseEvaluationEvents userName={userName} courseId={courseId} />
+          </div>
           <div></div>
         </div>
       </div>
