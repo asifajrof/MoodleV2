aad7686087fcf34a7799276ba222812241d61147
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2022-08-29 05:09:06 +0600

    extra class request done

--- moodlev2-client-react-app/src/teacher/components/TeacherAddNewRescheduleForm.js
+++ moodlev2-client-react-app/src/teacher/components/TeacherAddNewRescheduleForm.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import {
-	TextField,
-	Button,
-	InputLabel,
-	MenuItem,
-	ToggleButton,
-	ToggleButtonGroup,
+  TextField,
+  Button,
+  InputLabel,
+  MenuItem,
+  ToggleButton,
+  ToggleButtonGroup,
 } from "@mui/material";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { Select as SelectMui } from "@mui/material";
@@ -13,218 +13,272 @@ import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { ListItemIcon, ListItemText, Checkbox } from "@mui/material";
 import ShowScheduleReschedule from "./ShowScheduleReschedule";
 
 const typeListDummy = [
-	{
-		type_id: 1,
-		type_name: "Extra Class",
-	},
-	{
-		type_id: 2,
-		type_name: "Cancel Class",
-	},
+  {
+    type_id: 1,
+    type_name: "Extra Class",
+  },
+  {
+    type_id: 2,
+    type_name: "Cancel Class",
+  },
 ];
 
 const CourseEventsAddForm = ({ userName, courseId }) => {
-	//   const classes = useStyles();
-	const navigate = useNavigate();
-	const [typeList, setTypeList] = useState(typeListDummy);
-	const [sectionList, setSectionList] = useState([]);
-	const [rescheduleType, setRescheduleType] = useState("");
-	const [rescheduleTime, setRescheduleTime] = useState(new Date());
-	// console.log("typeList", typeList);
-	// fetch type list -> reschedule type
-	// currently fixed -> if problem let it be fixed
+  //   const classes = useStyles();
+  const navigate = useNavigate();
+  const [typeList, setTypeList] = useState(typeListDummy);
+  const [sectionList, setSectionList] = useState([]);
+  const [rescheduleType, setRescheduleType] = useState("");
+  const [rescheduleTime, setRescheduleTime] = useState(new Date());
+  const [eventSection, setEventSection] = useState([]);
 
-	//  useEffect(() => {
-	//     const fetchData = async () => {
-	//       try {
-	//         const response = await fetch(`/api/calendar/event_type`);
-	//         const jsonData = await response.json();
-	//         setTypeList(jsonData.data);
-	//       } catch (err) {
-	//         console.log(err);
-	//       }
-	//     };
-	//     fetchData();
-	//   }, []);
+  const isAllSelected =
+    sectionList.length > 0 && eventSection.length === sectionList.length;
 
-	useEffect(() => {
-		const fetchData = async (courseId) => {
-			try {
-				const response = await fetch(`/api/calendar/section_list/${courseId}`);
-				const jsonData = await response.json();
-				setSectionList(jsonData.data);
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		fetchData(courseId);
-	}, []);
+  useEffect(() => {
+    const fetchData = async (courseId) => {
+      try {
+        const response = await fetch(`/api/calendar/section_list/${courseId}`);
+        const jsonData = await response.json();
+        setSectionList(jsonData.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData(courseId);
+  }, []);
 
-	// const addCourseEvent = async (courseEventObj) => {
-	//   //post method here
-	//   //courseId, userName (for teacher info) available. other stuffs are input from form
-	//   try {
-	//     const res = await fetch(`/api/course/addNewEvent`, {
-	//       method: "POST",
-	//       headers: {
-	//         "Content-type": "application/json",
-	//       },
-	//       body: JSON.stringify(courseEventObj),
-	//     });
-	//     const jsonData = await res.json();
-	//     let id = null;
-	//     // console.log(data);
-	//     // console.log(res.status);
-	//     console.log(res);
-	//     if (res.status === 200) {
-	//       console.log(jsonData.message);
-	//       navigate(`/course/${courseId}/events`);
-	//     } else {
-	//       // alert(data.message);
-	//       console.log(jsonData.message);
-	//     }
-	//   } catch (err) {
-	//     console.log(err);
-	//     // alert(err);
-	//   }
-	// };
+  const eventSectionChange = (event) => {
+    const value = event.target.value;
+    if (value[value.length - 1] === "all") {
+      setEventSection(
+        eventSection.length === sectionList.length ? [] : sectionList // sectionList.map((s) => s.section_no)
+      );
+      return;
+    }
+    setEventSection(value);
+  };
 
-	const addCancelClass = async (cancelClassObj) => {
-		try {
-			const res = await fetch(`/api/reschedule/add/cancel`, {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json",
-				},
-				body: JSON.stringify(cancelClassObj),
-			});
-			const jsonData = await res.json();
-			if (res.status === 200) {
-				console.log(jsonData.message);
-				navigate(`/course/${courseId}/reschedule`);
-			} else {
-				console.log(jsonData.message);
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const addExtraClass = async (extraClassObj) => {
+    // console.log(extraClassObj);
+    // return;
+    //post method here
+    //courseId, userName (for teacher info) available. other stuffs are input from form
+    try {
+      const res = await fetch(`/api/reschedule/add/extra`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(extraClassObj),
+      });
+      const jsonData = await res.json();
+      if (res.status === 200) {
+        console.log(jsonData.message);
+        navigate(`/course/${courseId}/reschedule`);
+      } else {
+        console.log(jsonData.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	const onSubmitAction = (event) => {
-		event.preventDefault();
-		if (rescheduleType === 1) {
-			console.log("extra class on submit");
-		} else if (rescheduleType === 2) {
-			console.log("cancel class on submit");
-			const cancelClassObj = {
-				userName: userName,
-				reschTime: rescheduleTime,
-			};
-			console.log(cancelClassObj);
-			addCancelClass(cancelClassObj);
-		}
-		// const eventSectionList = eventSection.map((s) => s.secno);
-		// const courseEventObj = {
-		//   eventType: eventType,
-		//   eventFullMarks: eventFullMarks,
-		//   eventTime: eventTime,
-		//   eventDescription: eventDescription,
-		//   courseId: courseId,
-		//   teacherUserName: userName,
-		//   eventSectionList: eventSectionList,
-		// };
-		// console.log(courseEventObj);
-		// addCourseEvent(courseEventObj);
-		// console.log("onSubmitAction of add new course event");
-		return;
-	};
-	const [scheduleShow, setScheduleShow] = useState("");
-	const onScheduleShowChange = (event, newValue) => {
-		setScheduleShow(newValue);
-	};
+  const addCancelClass = async (cancelClassObj) => {
+    try {
+      const res = await fetch(`/api/reschedule/add/cancel`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(cancelClassObj),
+      });
+      const jsonData = await res.json();
+      if (res.status === 200) {
+        console.log(jsonData.message);
+        navigate(`/course/${courseId}/reschedule`);
+      } else {
+        console.log(jsonData.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	return (
-		<div className="addcourse__container">
-			<div className="addcourse__form__container">
-				<form onSubmit={onSubmitAction} style={{ width: "100%" }}>
-					<InputLabel id="addevent__select__type__label">
-						Select Type
-					</InputLabel>
-					<SelectMui
-						fullWidth
-						id="addevent__select__type"
-						label="Select Event Type"
-						value={rescheduleType}
-						onChange={(e) => setRescheduleType(e.target.value)}
-						required
-					>
-						{typeList.map((type, index) => {
-							return (
-								<MenuItem key={index} value={type.type_id}>
-									{type.type_name}
-								</MenuItem>
-							);
-						})}
-					</SelectMui>
-					<br />
-					<br />
-					{rescheduleType === "" ? (
-						<></>
-					) : (
-						<>
-							{rescheduleType === 2 ? (
-								<>
-									<div className="event__add__see__schedule">
-										<ToggleButtonGroup
-											color="primary"
-											value={scheduleShow}
-											exclusive
-											onChange={onScheduleShowChange}
-											aria-label="Platform"
-										>
-											<ToggleButton value="schedule">See Schedule</ToggleButton>
-										</ToggleButtonGroup>
-										<br /> <br />
-										{scheduleShow === "schedule" && (
-											<>
-												<ShowScheduleReschedule
-													userName={userName}
-													courseId={courseId}
-													sectionList={sectionList}
-													rescheduleType={rescheduleType}
-												/>
-												<br /> <br />
-											</>
-										)}
-									</div>
-									<LocalizationProvider dateAdapter={AdapterDayjs}>
-										<DateTimePicker
-											renderInput={(props) => <TextField {...props} />}
-											label="Select Time"
-											value={rescheduleTime}
-											onChange={(newValue) => {
-												setRescheduleTime(newValue);
-											}}
-										/>
-									</LocalizationProvider>
-									<br />
-									<br />
-									<Button type="submit" variant="outlined">
-										Submit
-									</Button>
-								</>
-							) : (
-								<></>
-							)}
-						</>
-					)}
-				</form>
-			</div>
-		</div>
-	);
+  const onSubmitAction = (event) => {
+    event.preventDefault();
+    if (rescheduleType === 1) {
+      // console.log("extra class on submit");
+      const eventSectionList = eventSection.map((s) => s.secno);
+      const extraClassObj = {
+        eventSectionList: eventSectionList,
+        eventTime: rescheduleTime,
+        userName: userName,
+      };
+      addExtraClass(extraClassObj);
+    } else if (rescheduleType === 2) {
+      // console.log("cancel class on submit");
+      const cancelClassObj = {
+        userName: userName,
+        reschTime: rescheduleTime,
+      };
+      console.log(cancelClassObj);
+      addCancelClass(cancelClassObj);
+    }
+    // const eventSectionList = eventSection.map((s) => s.secno);
+    // const courseEventObj = {
+    //   eventType: eventType,
+    //   eventFullMarks: eventFullMarks,
+    //   eventTime: eventTime,
+    //   eventDescription: eventDescription,
+    //   courseId: courseId,
+    //   teacherUserName: userName,
+    //   eventSectionList: eventSectionList,
+    // };
+    // console.log(courseEventObj);
+    // addCourseEvent(courseEventObj);
+    // console.log("onSubmitAction of add new course event");
+    return;
+  };
+  const [scheduleShow, setScheduleShow] = useState("");
+  const onScheduleShowChange = (event, newValue) => {
+    setScheduleShow(newValue);
+  };
+
+  return (
+    <div className="addcourse__container">
+      <div className="addcourse__form__container">
+        <form onSubmit={onSubmitAction} style={{ width: "100%" }}>
+          <InputLabel id="addevent__select__type__label">
+            Select Type
+          </InputLabel>
+          <SelectMui
+            fullWidth
+            id="addevent__select__type"
+            label="Select Event Type"
+            value={rescheduleType}
+            onChange={(e) => setRescheduleType(e.target.value)}
+            required
+          >
+            {typeList.map((type, index) => {
+              return (
+                <MenuItem key={index} value={type.type_id}>
+                  {type.type_name}
+                </MenuItem>
+              );
+            })}
+          </SelectMui>
+          <br />
+          <br />
+          {rescheduleType === "" ? (
+            <></>
+          ) : (
+            <>
+              {rescheduleType === 1 && (
+                <>
+                  <InputLabel id="mutiple-select-label">
+                    Select Section
+                  </InputLabel>
+                  <SelectMui
+                    fullWidth
+                    labelId="mutiple-select-label"
+                    multiple
+                    value={eventSection}
+                    onChange={eventSectionChange}
+                    renderValue={(eventSection) => {
+                      let str = "";
+                      eventSection.forEach((section) => {
+                        str += section.secname + ", ";
+                      });
+                      return str;
+                    }}
+                    // MenuProps={MenuProps}
+                  >
+                    <MenuItem
+                      value="all"
+                      //   classes={{
+                      //     root: isAllSelected ? classes.selectedAll : "",
+                      //   }}
+                    >
+                      <ListItemIcon>
+                        <Checkbox
+                          //   classes={{ indeterminate: classes.indeterminateColor }}
+                          checked={isAllSelected}
+                          indeterminate={
+                            eventSection.length > 0 &&
+                            eventSection.length < sectionList.length
+                          }
+                        />
+                      </ListItemIcon>
+                      <ListItemText
+                        // classes={{ primary: classes.selectAllText }}
+                        primary="Select All"
+                      />
+                    </MenuItem>
+                    {sectionList.map((option, index) => (
+                      <MenuItem key={index} value={option}>
+                        <ListItemIcon>
+                          <Checkbox
+                            checked={eventSection.indexOf(option) > -1}
+                          />
+                        </ListItemIcon>
+                        <ListItemText primary={option.secname} />
+                      </MenuItem>
+                    ))}
+                  </SelectMui>
+                  <br /> <br />
+                </>
+              )}
+              <div className="event__add__see__schedule">
+                <ToggleButtonGroup
+                  color="primary"
+                  value={scheduleShow}
+                  exclusive
+                  onChange={onScheduleShowChange}
+                  aria-label="Platform"
+                >
+                  <ToggleButton value="schedule">See Schedule</ToggleButton>
+                </ToggleButtonGroup>
+                <br /> <br />
+                {scheduleShow === "schedule" && (
+                  <>
+                    <ShowScheduleReschedule
+                      userName={userName}
+                      courseId={courseId}
+                      sectionList={
+                        rescheduleType === 1 ? eventSection : sectionList
+                      }
+                      rescheduleType={rescheduleType}
+                    />
+                    <br /> <br />
+                  </>
+                )}
+              </div>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateTimePicker
+                  renderInput={(props) => <TextField {...props} />}
+                  label="Select Time"
+                  value={rescheduleTime}
+                  onChange={(newValue) => {
+                    setRescheduleTime(newValue);
+                  }}
+                />
+              </LocalizationProvider>
+              <br />
+              <br />
+              <Button type="submit" variant="outlined">
+                Submit
+              </Button>
+            </>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CourseEventsAddForm;
--- moodlev2-server-app/controllers/reschedule-controllers.js
+++ moodlev2-server-app/controllers/reschedule-controllers.js
@@ -5,106 +5,106 @@ const moment = require("moment");
 const e = require("express");
 
 const getRescheduleEvents = async (req, res, next) => {
-	try {
-		const { courseId, userName } = req.body;
-		// const uId = req.params.uId;
-		console.log("GET api/reschedule/view");
-		let reschList = [];
-		let result = await pool.query(
-			"SELECT json_agg(t) FROM get_extra_class_teacher($1, $2) as t",
-			[userName, courseId]
-		);
-		// console.log(result.rows[0].json_agg);
-		const extra = result.rows[0].json_agg;
-		if (extra != null) {
-			for (c of extra) {
-				const obj = {
-					eventType: "Extra Class",
-					eventStartTime: c.start_time,
-					eventEndTime: c.end_time,
-					eventSectionName: c.sectionname,
-					eventTeacherName: c.teachername,
-				};
-				reschList.push(obj);
-			}
-		}
+  try {
+    const { courseId, userName } = req.body;
+    // const uId = req.params.uId;
+    console.log("GET api/reschedule/view");
+    let reschList = [];
+    let result = await pool.query(
+      "SELECT json_agg(t) FROM get_extra_class_teacher($1, $2) as t",
+      [userName, courseId]
+    );
+    // console.log(result.rows[0].json_agg);
+    const extra = result.rows[0].json_agg;
+    if (extra != null) {
+      for (c of extra) {
+        const obj = {
+          eventType: "Extra Class",
+          eventStartTime: c.start_time,
+          eventEndTime: c.end_time,
+          eventSectionName: c.sectionname,
+          eventTeacherName: c.teachername,
+        };
+        reschList.push(obj);
+      }
+    }
 
-		result = await pool.query(
-			"SELECT json_agg(t) FROM get_cancel_class_teacher($1, $2) as t",
-			[userName, courseId]
-		);
-		// console.log(result.rows[0].json_agg);
+    result = await pool.query(
+      "SELECT json_agg(t) FROM get_cancel_class_teacher($1, $2) as t",
+      [userName, courseId]
+    );
+    // console.log(result.rows[0].json_agg);
 
-		const cancel = result.rows[0].json_agg;
-		// console.log(result.rows[0].json_agg);
+    const cancel = result.rows[0].json_agg;
+    // console.log(result.rows[0].json_agg);
 
-		if (cancel != null) {
-			// console.log(cancel);
-			for (c of cancel) {
-				// console.log("==========================");
-				const obj = {
-					eventType: "Cancelled Class",
-					eventStartTime: c.start_time,
-					eventEndTime: c.end_time,
-					eventSectionName: c.sectionname,
-					eventTeacherName: c.teachername,
-				};
-				reschList.push(obj);
-			}
-		}
-		// console.log(reschList);
+    if (cancel != null) {
+      // console.log(cancel);
+      for (c of cancel) {
+        // console.log("==========================");
+        const obj = {
+          eventType: "Cancelled Class",
+          eventStartTime: c.start_time,
+          eventEndTime: c.end_time,
+          eventSectionName: c.sectionname,
+          eventTeacherName: c.teachername,
+        };
+        reschList.push(obj);
+      }
+    }
+    // console.log(reschList);
 
-		res.json({ message: "getRescheduleEvents", data: reschList });
-	} catch (err) {
-		return next(new HttpError(err.message, 500));
-	}
+    res.json({ message: "getRescheduleEvents", data: reschList });
+  } catch (err) {
+    return next(new HttpError(err.message, 500));
+  }
 };
 
 const addCancelClass = async (req, res, next) => {
-	try {
-		const { userName, reschTime } = req.body;
-		console.log("POST api/reschedule/add/cancel");
-		const result = await pool.query("SELECT cancel_class($1, $2, $3)", [
-			userName,
-			moment(reschTime).format("YYYY-MM-DD"),
-			moment(reschTime).format("HH:mm"),
-		]);
-		res.json({
-			message: "addCancelClass",
-			data: [],
-		});
-	} catch (err) {
-		return next(new HttpError(err.message, 500));
-	}
+  try {
+    const { userName, reschTime } = req.body;
+    console.log("POST api/reschedule/add/cancel");
+    const result = await pool.query("SELECT cancel_class($1, $2, $3)", [
+      userName,
+      moment(reschTime).format("YYYY-MM-DD"),
+      moment(reschTime).format("HH:mm"),
+    ]);
+    res.json({
+      message: "addCancelClass",
+      data: [],
+    });
+  } catch (err) {
+    return next(new HttpError(err.message, 500));
+  }
 };
 
 const addExtraClass = async (req, res, next) => {
-	try {
-		const { eventSectionList, eventTime, userName } = req.body;
+  try {
+    const { eventSectionList, eventTime, userName } = req.body;
 
-		const eventTimeMoment = moment(eventTime);
-		const givenTime = eventTimeMoment.format("YYYY-MM-DD HH:mm:ssZZ");
-		// console.log(givenTime);
-		const givenTimePlusOneHour = eventTimeMoment
-			.add(1, "hours")
-			.format("YYYY-MM-DD HH:mm:ssZZ");
-		// const currentTime = moment().format("YYYY-MM-DD HH:mm:ssZZ");
-		console.log(currentTime);
+    const eventTimeMoment = moment(eventTime);
+    const givenTime = eventTimeMoment.format("YYYY-MM-DD HH:mm:ssZZ");
+    // console.log(givenTime);
+    const givenTimePlusOneHour = eventTimeMoment
+      .add(1, "hours")
+      .format("YYYY-MM-DD HH:mm:ssZZ");
+    // const currentTime = moment().format("YYYY-MM-DD HH:mm:ssZZ");
+    // console.log(currentTime);
 
-		for (sec of eventSectionList) {
-			let result = await pool.query(
-				"select json_agg(t) FROM add_extra_class_request($1,$2,$3,$4) as t",
-				[sec, userName, givenTime, givenTimePlusOneHour]
-			);
+    for (sec of eventSectionList) {
+      let result = await pool.query(
+        "select json_agg(t) FROM add_extra_class_request($1,$2,$3,$4) as t",
+        [sec, userName, givenTime, givenTimePlusOneHour]
+      );
 
-			// const evaluationId = result.rows[0].json_agg;
-			// console.log(result);
-		}
+      // const evaluationId = result.rows[0].json_agg;
+      // console.log(result);
+    }
 
-		res.json({ message: "new extra class requested", data: [] });
-	} catch (err) {
-		return next(new HttpError(err.message, 500));
-	}
+    res.json({ message: "new extra class requested", data: [] });
+  } catch (err) {
+    return next(new HttpError(err.message, 500));
+  }
 };
 exports.getRescheduleEvents = getRescheduleEvents;
 exports.addCancelClass = addCancelClass;
