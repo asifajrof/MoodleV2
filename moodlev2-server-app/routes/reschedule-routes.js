41002934c339d023eb9254504a93c240246e518c
fatimanawmi <fatimanawmi@gmail.com>
2022-08-29 06:26:57 +0600

    resch req info

--- moodlev2-server-app/routes/reschedule-routes.js
+++ moodlev2-server-app/routes/reschedule-routes.js
@@ -7,5 +7,6 @@ const rescheduleRoutes = express.Router();
 rescheduleRoutes.post("/view", rescheduleControllers.getRescheduleEvents); //
 rescheduleRoutes.post("/add/extra", rescheduleControllers.addExtraClass);
 rescheduleRoutes.post("/add/cancel", rescheduleControllers.addCancelClass);
+rescheduleRoutes.post("/info/extra", rescheduleControllers.getExtraClassInfo);
 
 module.exports = rescheduleRoutes;
