f52996d4bf74cf445a00c336a4af2874801a003a
fatimanawmi <fatimanawmi@gmail.com>
2022-08-29 06:43:50 +0600

    confirm/reschedule backend

--- moodlev2-server-app/routes/reschedule-routes.js
+++ moodlev2-server-app/routes/reschedule-routes.js
@@ -8,5 +8,13 @@ rescheduleRoutes.post("/view", rescheduleControllers.getRescheduleEvents); //
 rescheduleRoutes.post("/add/extra", rescheduleControllers.addExtraClass);
 rescheduleRoutes.post("/add/cancel", rescheduleControllers.addCancelClass);
 rescheduleRoutes.post("/info/extra", rescheduleControllers.getExtraClassInfo);
+rescheduleRoutes.get(
+	"/confirmExtraClass/:eventId",
+	rescheduleControllers.confirmExtraClass
+);
+rescheduleRoutes.get(
+	"/rescheduleRequest/:eventId",
+	rescheduleControllers.rescheduleRequest
+);
 
 module.exports = rescheduleRoutes;
